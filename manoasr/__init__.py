397311a6c13e3fc7e3fed29cd39da0d8febb39ab
xiaoluo1009 <xiaoluo1009@users.noreply.github.com>
2026-06-02 19:13:21 +0800

    Bump version to 0.1.8

--- manoasr/__init__.py
+++ manoasr/__init__.py
@@ -1,4 +1,4 @@
 # coding=utf-8
 """mano-asr: 本地语音转写服务"""
 
-__version__ = "0.1.7"
+__version__ = "0.1.8"
--- manoasr/cli/utils/constants.py
+++ manoasr/cli/utils/constants.py
@@ -3,7 +3,7 @@
 
 from pathlib import Path
 
-VERSION = "0.1.7"
+VERSION = "0.1.8"
 
 DEFAULT_PORT = 8787
 
